4eabfc7bac503b53f68dc698cd29f75fac0e2b60
geobyjmh <38050735+geobyjmh@users.noreply.github.com>
2021-12-14 19:49:19 +0000

    Just changed the order of some of the functions.

--- JoinImages.py
+++ JoinImages.py
@@ -3,6 +3,15 @@ from PIL import Image
 COLUMNS = 4
 ROWS = 3
 
+def Main():
+    imageNames = FileToList('image_list.txt')  
+    imgs = OpenImages(imageNames)
+
+    blank_img = CreateNewBlankImage(imgs, ROWS, COLUMNS)
+    new_img = AddIndividualImagesToNewImage(blank_img, imgs)
+    scaled_img = ResizeImage(new_img)
+    scaled_img.save('NewImage.jpg')
+    
 def FileToList(path):
     file = open(path)
     img_names = [line.strip() for line in file]
@@ -19,15 +28,6 @@ def CreateNewBlankImage(images, row, col):
     newImage = Image.new('RGB', (imageWidth, imageHeight))
     return newImage
 
-def IncXandY(xpos, ypos, xstep, ystep, xmax):
-    next_x = xpos + xstep
-    next_y = ypos
-    if next_x >= xmax:
-        next_x = 0
-        next_y = ypos + ystep
-    return next_x, next_y
-        
-    
 def AddIndividualImagesToNewImage(work_image, images):
     maxWidth = work_image.size[0]
     x = 0
@@ -37,7 +37,15 @@ def AddIndividualImagesToNewImage(work_image, images):
         x, y = IncXandY(x, y, img.size[0], img.size[1], maxWidth)     
     return work_image
 
-            
+def IncXandY(xpos, ypos, xstep, ystep, xmax):
+    next_x = xpos + xstep
+    next_y = ypos
+    if next_x >= xmax:
+        next_x = 0
+        next_y = ypos + ystep
+    return next_x, next_y
+        
+
 def ResizeImage(image):
     scale = 10
     width = int(image.size[0]/scale)
@@ -45,14 +53,6 @@ def ResizeImage(image):
     resizedImage = image.resize((width, height))
     return resizedImage
 
-def Main():
-    imageNames = FileToList('image_list.txt')  
-    imgs = OpenImages(imageNames)
-
-    blank_img = CreateNewBlankImage(imgs, ROWS, COLUMNS)
-    new_img = AddIndividualImagesToNewImage(blank_img, imgs)
-    scaled_img = ResizeImage(new_img)
-    scaled_img.save('NewImage.jpg')
     
 if __name__ == '__main__':
     Main()
